6ce91c9d00dca5ed0347b37dba4577c8b70a6865
arpruss <arpruss@gmail.com>
2023-10-14 08:52:13 -0500

    foo

--- generate-mame2003.sh
+++ generate-mame2003.sh
@@ -5,7 +5,7 @@ python3 makezip.py -o mame2003 extracted/Galaxian.rom galaxian.xml
 python3 makezip.py -o mame2003 extracted/Mappy.rom mappy.xml
 python3 makezip.py -o mame2003 extracted/RallyX.rom rallyx.xml rallyxn.1e 0x1000 rallyxn.1k 0x3000 rx1-2.4n -1 rx1-3.7k -1 rx1-4.2m -1
 python3 makezip.py -o mame2003 extracted/SkyKid.rom skykid.xml
-python3 makezip-separate.py -o mame2003 dspirit.xml extracted/DR*ROM extracted/*MCU*ROM
+python3 makezip-separate.py -o mame2003 dspirit.c extracted/DR*ROM extracted/*MCU*ROM
 python3 makezip-separate.py -o mame2003 galaga88.xml extracted/G8* extracted/*MC*
 python3 makezip-separate.py -o mame2003 pacmania.xml extracted/PM* extracted/*MC*
 python3 makezip-separate.py -o mame2003 rthunder.c extracted/*
